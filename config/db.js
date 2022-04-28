74b77f7a2e8854ed4cc44bd2c4b577cf93792a85
jackson916 <colingrey412@gmail.com>
2022-04-28 18:23:55 -0400

    commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'Game-review'
+const mongooseBaseName = 'Retro-Game-Review'
 
 // create the mongodb uri for development and test
 const database = {
